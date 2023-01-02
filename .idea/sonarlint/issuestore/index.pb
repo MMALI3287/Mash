a33952f199e53d265f69ed3e8718c0a8395fba7f
MD Musaddique Ali Erfan <musaddique.cse.20200204049@aust.edu>
2023-01-02 21:49:58 +0600

    Initial

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,34 @@
 
 @
 local.properties,0\7\0712df971a99ac4d2fccb8e0fb19f377f3374cca
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+A
+gradle.properties,2\a\2afbb999f001938c88fa43fc2ef52abf0f8213e4
+?
+settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
+7
+gradlew,5\b\5bbfa66edb4db3c7c33c5181f43510990d3307f9
+X
+(gradle/wrapper/gradle-wrapper.properties,f\b\fbe448ebfc3eb2d4e308f6b8b043666f5b57235e
+;
+gradlew.bat,2\a\2a45a911a8f1836b0b6c5b758962572012d8f8c3
+>
+app/.gitignore,5\1\51e1c5d383dfaa35e0e7e5873a0a99355a86880f
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+F
+app/proguard-rules.pro,9\e\9e08934d811afe28fbc77aaa3c0d747b94348db9
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+v
+Fapp/src/androidTest/java/com/example/mash/ExampleInstrumentedTest.java,9\e\9eafbcfdf04602c23a98a9f02bc3a914ab41f3d1
+g
+7app/src/test/java/com/example/mash/ExampleUnitTest.java,6\b\6bc112294302b9e30e551974f620cee451045970
+d
+4app/src/main/java/com/example/mash/MainActivity.java,1\5\15195205b3e92f152443749ef6ee33b4a2c13a37
--- app/src/main/java/com/example/mash/MainActivity.java
+++ app/src/main/java/com/example/mash/MainActivity.java
@@ -9,9 +9,7 @@ import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 public class MainActivity extends AppCompatActivity {
-
 
     Button button;
     TextView textView;
@@ -23,35 +21,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       button= (Button) findViewById(R.id.CLICK);
-        textView=(TextView) findViewById(R.id.Result);
-      checkBox1=(CheckBox) findViewById(R.id.Japan);
-       checkBox2=(CheckBox) findViewById(R.id.England);
-       checkBox3= (CheckBox)findViewById(R.id.Morocco);
+        button = (Button) findViewById(R.id.CLICK);
+        textView = (TextView) findViewById(R.id.Result);
+        checkBox1 = (CheckBox) findViewById(R.id.Japan);
+        checkBox2 = (CheckBox) findViewById(R.id.England);
+        checkBox3 = (CheckBox) findViewById(R.id.Morocco);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String result="";
-                if(checkBox1.isChecked()&&checkBox2.isChecked()){
-                    Toast.makeText(MainActivity.this,"You have to select any one option",Toast.LENGTH_SHORT).show();
-                }
-                else if(checkBox1.isChecked()&&checkBox3.isChecked()){
-                    Toast.makeText(MainActivity.this,"You have to select any one option",Toast.LENGTH_SHORT).show();
-                }
-                else if(checkBox2.isChecked()&&checkBox3.isChecked()){
-                    Toast.makeText(MainActivity.this,"You have to select any one option",Toast.LENGTH_SHORT).show();
-                }
-                else if(checkBox1.isChecked()&&checkBox2.isChecked()&&checkBox3.isChecked()){
-                    Toast.makeText(MainActivity.this,"You have to select any one option",Toast.LENGTH_SHORT).show();
-                }
-                else if(checkBox1.isChecked()||checkBox2.isChecked()){
-                    result+="Wrong Answer";
-                }
-                else if(checkBox3.isChecked()){
-                    result+="Correct Answer";
-                }
-                else{
-                    Toast.makeText(MainActivity.this,"You have to select at least one option",Toast.LENGTH_SHORT).show();
+                String result = "";
+                String mulString = "You have to select any one option";
+                if (checkBox1.isChecked() && checkBox2.isChecked()) {
+                    Toast.makeText(MainActivity.this, mulString, Toast.LENGTH_SHORT).show();
+                } else if (checkBox1.isChecked() && checkBox3.isChecked()) {
+                    Toast.makeText(MainActivity.this, mulString, Toast.LENGTH_SHORT).show();
+                } else if (checkBox2.isChecked() && checkBox3.isChecked()) {
+                    Toast.makeText(MainActivity.this, mulString, Toast.LENGTH_SHORT).show();
+                } else if (checkBox1.isChecked() && checkBox2.isChecked() && checkBox3.isChecked()) {
+                    Toast.makeText(MainActivity.this, mulString, Toast.LENGTH_SHORT).show();
+                } else if (checkBox1.isChecked() || checkBox2.isChecked()) {
+                    result += "Wrong Answer";
+                } else if (checkBox3.isChecked()) {
+                    result += "Correct Answer";
+                } else {
+                    Toast.makeText(MainActivity.this, "You have to select at least one option", Toast.LENGTH_SHORT)
+                            .show();
                 }
                 textView.setText(result);
             }
